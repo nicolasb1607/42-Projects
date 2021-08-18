2aaca255605cdc222af73b64537131ecaf4b8b07
Nicolas BURAT DE GURGY <nicolasburat1607@gmail.com>
2021-08-18 13:14:10 +0200

    maj c06

---
+++ c07/ex03/ft_strjoin.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+
+	char **ptr_strs;
+	
+	ptr_strs = (char *) malloc(size * sizeof (char)); 
+	
+
+		
+}
+
+int main(void)
+{
+	int size = 18; //size represente la taille de strs
+	char str[0][0] = "Salut";// 5
+	char str[1][0] = "comment"; //7
+	char str[2][0] = "ca";// 2
+	char str[3][0] = "va"; // 2
+	char str[4][0] = "?"; // 1
+	char sep[] = '/';
+
+	
+	ft_strjoin(size, &strs, &sep)
+	return (0);
+}
