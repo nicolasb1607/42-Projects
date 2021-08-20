dab5a514b9a7ca87f8214da97ec5dc95041eb58f
Nicolas Burat de gurgy <nicolasburat1607@gmail.com>
2021-08-20 10:10:42 +0200

    norminette ok sur c06

--- c06/ex02/ft_rev_params.c
+++ c06/ex02/ft_rev_params.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nburat-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/20 09:59:56 by nburat-d          #+#    #+#             */
+/*   Updated: 2021/08/20 10:02:56 by nburat-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include <unistd.h>
 
 void	ft_putstr(char *str)
@@ -15,23 +26,20 @@ void	ft_putstr(char *str)
 int	ft_len(char **str)
 {
 	int	i;
-	
+
 	i = 0;
 	while (str[i])
-	{
 		i++;
-	}
 	return (i);
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-	
 	argc = ft_len(argv);
 	while (argc > 1)
 	{
 		ft_putstr(argv[argc - 1]);
-		write(1 , "\n", 1);
+		write(1, "\n", 1);
 		argc--;
 	}
 	return (0);
