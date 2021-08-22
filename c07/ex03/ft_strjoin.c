6bad3b269f34ea03ff868a2e506ce930caac30ec
Nicolas Burat de gurgy <nicolasburat1607@gmail.com>
2021-08-22 11:39:02 +0200

    c07 ex03 ok

--- c07/ex03/ft_strjoin.c
+++ c07/ex03/ft_strjoin.c
@@ -1,5 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nburat-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/22 11:37:00 by nburat-d          #+#    #+#             */
+/*   Updated: 2021/08/22 11:38:01 by nburat-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include <stdlib.h>
 #include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
 
 int	ft_getlen(char **str, char *sep, int size)
 {
@@ -13,15 +34,10 @@ int	ft_getlen(char **str, char *sep, int size)
 	total_len = 0;
 	while (i < size)
 	{
-		j = 0;
-		while (str[i][j])
-		{
-			total_len++;
-			j++;
-		}
+		total_len = total_len + ft_strlen(str[i]);
 		i++;
 	}
-	while (len_argc < size - 1)		
+	while (len_argc < size - 1)
 	{	
 		j = 0;
 		while (sep[j])
@@ -42,7 +58,7 @@ void	ft_strcat(char *s1, char *s2)
 	j = 0;
 	i = 0;
 	while (s1[i])
-			i++;
+		i++;
 	while (s2[j])
 	{
 		s1[i] = s2[j];
@@ -56,32 +72,24 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 {
 	char	*strconcat;
 	int		i;
+
 	if (size >= 2)
 	{
 		i = 1;
-		strconcat = malloc(ft_getlen(strs, sep, size) * sizeof(char));
+		strconcat = malloc(ft_getlen(strs, sep, size) * sizeof (char));
+		strconcat[0] = '\0';
 		while (i < size)
 		{	
-			ft_strcat(strconcat,strs[i]);
+			ft_strcat(strconcat, strs[i]);
 			if (i < size - 1)
-			{
-				ft_strcat(strconcat, strs[i]);
-			}
+				ft_strcat(strconcat, sep);
 			i++;
 		}
-	
 	}
 	if (size == 1)
 	{
-		strconcat = malloc(1 * sizeof(char));
+		strconcat = malloc(1 * sizeof (char));
 		strconcat[0] = '\0';
 	}
 	return (strconcat);
-}
-int main(int argc, char *argv[])
-{
-	char sep[] = "/?";
-
-	printf("%s\n", ft_strjoin(argc, argv, sep));	
-	return (0);
 }
