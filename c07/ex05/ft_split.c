7ddefc3837b87431addae5f30e7e6aa8ca91ace2
Nicolas BURAT DE GURGY <nicolasburat1607@gmail.com>
2021-08-25 21:34:32 +0200

    split OK

--- c07/ex05/ft_split.c
+++ c07/ex05/ft_split.c
@@ -51,7 +51,6 @@ int	ft_word_count(char *str, char *charset)
 		}
 		i++;
 	}	
-	printf("sortie de boucle\n");	
 	return (nb_word);
 }
 
@@ -61,40 +60,37 @@ char **ft_split(char *str, char *charset)
 	char	**ptr_tab;
 	int	i;
 	int	j;
-	int nb_word;
-	int k;
+	int	k;
+	int	len_word;
 
-	k = 0;
 	j = 0;
 	i = 0;
-	nb_word = ft_word_count(str, charset);
-	printf("DELIVRANCE\n");
-	ptr_tab = malloc((nb_word + 1) * sizeof (char*));
+	ptr_tab = malloc((ft_word_count(str, charset) + 1) * sizeof (char*));
 	if (!ptr_tab)
 		return (NULL);
-	while (i < nb_word)
+	while (i < ft_word_count(str, charset))
 	{
 		while (is_separator(str[j], charset) == 1)
-		{
 			j++;
-			printf("j = %d\n" , j);
-		}
-		printf("boucle Main\n");	
 		ptr_tab[i] = malloc((ft_strlen_word(str, charset, j) + 1) * sizeof(char));
 		len_word = ft_strlen_word(str,charset,j);
-		while (k < len_word)
+		k = 0;
+	 	while (k < len_word)
 		{
 			ptr_tab[i][k] = str[j];
 			k++;
-		}
- 		ptr_tab[i][j] = '\0';
+			j++;
+		}		
+		ptr_tab[i][k] = '\0';
 		i++;
 	}
 
  	ptr_tab[i] = NULL;
 	return (ptr_tab);
 }
-/*int main(void)
+
+/*
+int main(void)
 {	
 	int i;
 	char **ptr;
@@ -105,13 +101,12 @@ char **ft_split(char *str, char *charset)
 	ptr = ft_split(str, sep);
 	while (ptr[i])
 	{
-		printf("Main boucle");
 		printf("%s", ptr[i]);
 		i++;
 	}
 	return(0);
-}*/
-
+}
+*/
 
 int	main(int ac, char **av)
 {
@@ -121,7 +116,7 @@ int	main(int ac, char **av)
 
 	split = ft_split(av[1], av[2]);
 	i = 0;
-	while (i < ft_word_count(av[1], av[2]) + 1)
+	while (i < ft_word_count(av[1], av[2]))
 	{
 		printf("%s\n", split[i]);
 		i++;
