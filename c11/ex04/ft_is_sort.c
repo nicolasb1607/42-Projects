6acc8c1451615f3d8dad5f56ae3fb75b660d6502
Nicolas_XPS <nicolasburat1607@gmail.com>
2021-09-17 14:47:48 +0200

    C11 jusq'a ex04

---
+++ c11/ex04/ft_is_sort.c
@@ -0,0 +1,16 @@
+int	ft_is_sort(int *tab, int length, int(*f)(int, int))
+{
+
+	int i; 
+
+	i = 1; 
+
+	while (i < length)
+	{
+
+		if (f(tab[i-1],tab[i]) < 0)
+			return(0);
+		i++; 
+	}
+	return(1);
+}
